d86242fc60b4f0dec8eef233eef43e345d8f2ef7
joshuanatan <joshuanatan.jn@gmail.com>
2020-04-23 03:14:26 +0700

    Update Account.php
    done beresin bug session

--- control/application/controllers/Account.php
+++ control/application/controllers/Account.php
@@ -3,17 +3,22 @@ defined("BASEPATH") or exit("No Direct Script");
 class Account extends CI_Controller{
     public function __construct(){
         parent::__construct();
+        if(!$this->user_verification->is_logged_on($this->session->id_submit_acc)){
+            redirect("welcome/dashboard");
+        }
     }
     public function index(){
-        echo $this->session->id_submit_acc;
         $this->page_generator->head();
         $this->page_generator->page_open();
         $this->page_generator->navbar($this->session->id_submit_acc,"account");
+        
         $this->load->view("account/page_open");
         $this->load->model("m_account");
+        
         $data["col"] = $this->m_account->column();
-        $data["data"] = $this->m_account->list();
+        
         $this->load->view("account/content",$data);
+        
         $this->load->view("account/page_close");
         $this->page_generator->page_close();
     }
