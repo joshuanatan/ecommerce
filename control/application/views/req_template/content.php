7f558ef7b1af9e9853182fa083436c10a1679425
joshuanatan <joshuanatan.jn@gmail.com>
2020-04-24 13:12:18 +0700

    Update content.php

--- control/application/views/req_template/content.php
+++ control/application/views/req_template/content.php
@@ -47,14 +47,34 @@
                     </div>
                     <div class = "form-group">
                         <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
-                        <button type = "submit" class = "btn btn-sm btn-primary">Submit</button>
+                        <button type = "button" onclick = "register()" class = "btn btn-sm btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
-
+<div class = "modal fade" id = "update_dialog">
+    <div class = "modal-dialog">
+        <div class = "modal-content">
+            <div class = "modal-header">
+                <h4 class = "modal-title">modal_form_title</h4>
+            </div>
+            <div class = "modal-body">
+                <form action = "" method = "POST">
+                    <div class = "form-group">
+                        <h5>form_data_label</h5>
+                        <input type = "text" class = "form-control" required name = "">
+                    </div>
+                    <div class = "form-group">
+                        <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
+                        <button type = "button" onclick = "update()" class = "btn btn-sm btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <div class = "modal fade" id = "delete_dialog">
     <div class = "modal-dialog">
         <div class = "modal-content">
@@ -69,7 +89,7 @@
                     </table>
                     <div class = "row">
                         <button type = "button" class = "btn btn-sm btn-primary col-lg-3 col-sm-12 offset-lg-3" data-dismiss = "modal">Cancel</button>
-                        <button type = "submit" class = "btn btn-sm btn-danger col-lg-3">Delete</button>
+                        <button type = "button" onclick = "remove()" class = "btn btn-sm btn-danger col-lg-3">Delete</button>
                     </div>
                 </form>
             </div>
@@ -77,13 +97,32 @@
     </div>
 </div>
 <script>
+    var orderBy = 0;
+    var orderDirection = "ASC";
+    var searchKey = "";
     function refresh(page = 1) {
         $.ajax({
-            url: "<?php echo base_url() ?>",
+            url: "<?php echo base_url();?>f/account/list?orderBy="+orderBy+"&orderDirection="+orderDirection+"&page="+page+"&searchKey="+searchKey,
             type: "GET",
             dataType: "JSON",
             success: function(respond) {
                 var html = "";
+                if(respond["status"] == "SUCCESS"){
+                    for(var a = 0; a<respond["content"].length; a++){
+                        html += "<tr>";
+                        html += "<td class = 'align-middle text-center'></td>";
+                        html += "<td class = 'align-middle text-center'><i style = 'cursor:pointer;font-size:large' class = 'text-primary md-edit'></i> | <i style = 'cursor:pointer;font-size:large' class = 'text-danger md-delete'></i></td>";
+                        html += "</tr>";
+                    }
+                }
+                else{
+                    html += "<tr>";
+                    html += "<td colspan = 2 class = 'align-middle text-center'>No Records Found</td>";
+                    html += "</tr>";
+                }
+                $("#content_container").html(html);
+
+                html = "";
                 if(respond["page"]["previous"]){
                     html += '<li class="page-item"><a class="page-link" onclick = "refresh('+(respond["page"]["before"])+')"><</a></li>';
                 }
@@ -107,13 +146,81 @@
             },
             error: function(){
                 var html = "";
+                html += "<tr>";
+                html += "<td colspan = 2 class = 'align-middle text-center'>No Records Found</td>";
+                html += "</tr>";
+                $("#content_container").html(html);
+
+                html = "";
                 html += '<li class="page-item"><a class="page-link" style = "cursor:not-allowed"><</a></li>';
                 html += '<li class="page-item"><a class="page-link" style = "cursor:not-allowed">></a></li>';
                 $("#pagination_container").html(html);
             }
         });
     }
-    function sort(col_name){
+    function sort(colNum){
+        if(parseInt(colNum) != orderBy){
+            orderBy = colNum; 
+            orderDirection = "ASC";
+            var orderDirectionHtml = '<span class="badge badge-light align-top" id = "orderDirection">ASC</span>';
+            $("#orderDirection").remove();
+            $("#col"+colNum).append(orderDirectionHtml);
+        }
+        else{
+            var direction = $("#orderDirection").text();
+            if(direction == "ASC"){
+                orderDirection = "DESC";
+            }
+            else{
+                orderDirection = "ASC";
+            }
+            $("#orderDirection").text(orderDirection);
+        }
+        refresh();
+    }
+    function search(){
+        searchKey = $("#search_box").val();
+        refresh();
+    }
+</script>
+<script>
+    function register(){
+        $.ajax({
+            url:"<?php echo base_url();?>",
+            type:"POST",
+            dataType:"JSON",
+            data:{
+
+            },
+            success:function(respond){
+
+            }
+        });
+
+    }
+    function update(){
+        $.ajax({
+            url:"<?php echo base_url();?>",
+            type:"POST",
+            dataType:"JSON",
+            data:{
+
+            },
+            success:function(respond){
+
+            }
+        });
+
+    }
+    function remove(){
+        $.ajax({
+            url:"<?php echo base_url();?>",
+            type:"DELETE",
+            dataType:"JSON",
+            success:function(respond){
+
+            }
+        });
 
     }
 </script>
